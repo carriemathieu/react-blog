1a7b2dac06f30f3bbb3976a5a4c21dcecc510846
Carrie Mathieu <carrieburks@gmail.com>
2021-05-10 15:08:08 -0400

    renderposts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import postsReducer from './postsReducer'
 
+// adds a "posts" attribute to state
 export default combineReducers({
     posts: postsReducer
 })
