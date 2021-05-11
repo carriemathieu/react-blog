f09260331efba17887083752fbceb3db79b75dc0
Carrie Mathieu <carrieburks@gmail.com>
2021-05-10 23:13:02 -0400

    refactor fetchuser using memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 // dispatch - pass action into middleware to initiate changes on redux side
@@ -7,16 +8,12 @@ export const fetchPosts = () => async dispatch  => {
     dispatch ({ type: 'FETCH_POSTS', payload: response.data })
 }
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch)
+
+// private function
+// memoize prevents multiple fetch calls for same user ID
+const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${id}`)
 
     dispatch ({ type: 'FETCH_USER', payload: response.data})
-}
-
-// export const fetchPosts = () => {    
-//     return async function(dispatch, getState) {
-//         const response = await jsonPlaceholder.get('/posts')
-        
-//         dispatch ({ type: 'FETCH_POSTS', payload: response})
-//     }
-// }
+})
